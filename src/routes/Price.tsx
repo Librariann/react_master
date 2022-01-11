d1262b4b693b4f75ee904ecb736b6d0e8d4c4923
Librariann <okpc0306@naver.com>
2022-01-12 00:06:17 +0900

    price 추가중

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,32 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+
+interface IPrice {
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 function Price() {
-  return <h1>Price</h1>;
+  const { coinId } = useParams() as unknown as IPrice;
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return <div>{isLoading ? "price loading.." : <div>asdasd</div>}</div>;
 }
 
 export default Price;
